abc5eb63de89bcb866140233b353fe0c625f490b
pedrobazterrica <pedrobazterrica@hotmail.com>
2023-07-24 20:58:25 -0300

    Added a cache configuration file

--- src/main/java/com/tenpo/challenge/ChallengeApplication.java
+++ src/main/java/com/tenpo/challenge/ChallengeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChallengeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
