210d6d5202b6476beedc498e5f44cfc943340482
pedrobazterrica <pedrobazterrica@hotmail.com>
2023-07-24 20:54:39 -0300

    Forgot to add docker-compose on last commit. Added basic datasource properties for postgresql, added a rest controller to use as an api, added some tests for controller. Added cache dependency in pom and fixed jar name so that it is easier to use docker

--- src/main/java/com/tenpo/challenge/controller/CalculatorController.java
+++ src/main/java/com/tenpo/challenge/controller/CalculatorController.java
@@ -1,2 +1,23 @@
-package com.tenpo.challenge.controller;public class CalculatorController {
+package com.tenpo.challenge.controller;
+
+import com.tenpo.challenge.service.CalculatorService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/calculate/numbers/{number1}/{number2}")
+    public Double getValue(@PathVariable("number1") @Validated Double number1, @PathVariable("number2") @Validated Double number2){
+        return calculatorService.calculateValue(number1,number2);
+    }
 }
--- src/test/java/com/tenpo/challenge/controller/CalculatorControllerTest.java
+++ src/test/java/com/tenpo/challenge/controller/CalculatorControllerTest.java
@@ -1,2 +1,45 @@
-package com.tenpo.challenge.controller;public class CalculatorControllerTest {
+package com.tenpo.challenge.controller;
+
+import com.tenpo.challenge.service.CalculatorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = CalculatorController.class)
+public class CalculatorControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private CalculatorService calculatorService;
+
+    @Test
+    void whenValidInput_thenReturns200() throws Exception {
+        when(calculatorService.calculateValue(any(),any())).thenReturn(11d);
+        mockMvc.perform(get("/api/v1/calculate/numbers/5/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("11.0"));
+    }
+
+    @Test
+    void whenInvalidInput_shouldReturn400BadRequest() throws Exception {
+        mockMvc.perform(get("/api/v1/calculate/numbers/5/hola"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void whenInvalidInput_shouldReturn405InvalidMethod() throws Exception {
+        mockMvc.perform(post("/api/v1/calculate/numbers/5/5"))
+                .andExpect(status().is4xxClientError());
+    }
 }
