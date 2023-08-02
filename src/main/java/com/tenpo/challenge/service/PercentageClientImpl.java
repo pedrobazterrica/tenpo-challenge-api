40d00dee6b84bc68e0ac17f4ee1305626dd036bb
pedrobazterrica <pedrobazterrica@hotmail.com>
2023-08-02 19:08:26 -0300

    Added custom exception for percentageClient, this has many purposes including recognising where the exception was thrown in an easier way and handling retries better, also now calling a real rest service for our percentage

--- src/main/java/com/tenpo/challenge/service/PercentageClientImpl.java
+++ src/main/java/com/tenpo/challenge/service/PercentageClientImpl.java
@@ -1,6 +1,7 @@
 package com.tenpo.challenge.service;
 
 import com.tenpo.challenge.dto.PercentageDto;
+import com.tenpo.challenge.exception.PercentageClientException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -24,7 +25,12 @@ public class PercentageClientImpl implements PercentageClient {
 
     @Override
     public Double getPercentage() throws HttpServerErrorException, HttpClientErrorException {
-        log.info("Trying to get percentage from client: {}", url);
-        return restTemplate.getForObject(url, PercentageDto.class).getPercentage();
+        try{
+            log.info("Trying to get percentage from client: {}", url);
+            return restTemplate.getForObject(url, PercentageDto.class).getPercentage();
+        }catch(Exception e){
+            log.error("There was an error trying to get percentage from {}", url, e.getMessage());
+            throw new PercentageClientException("Percentage client is unavailable at the moment, please try again later.",e);
+        }
     }
 }
